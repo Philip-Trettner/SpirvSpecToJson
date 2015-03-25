e5e8b91050e3e422334e9dbe1ea186d0fe1c38a8
Marco Trettner <marco.trettner+bitbucket@googlemail.com>
2015-03-25 13:00:08 +0100

    Added methode to convert string into CamelCase

--- SpirvSpecToJson/SpirvSpecToJson/Extensions.cs
+++ SpirvSpecToJson/SpirvSpecToJson/Extensions.cs
@@ -21,7 +21,7 @@ namespace SpirvSpecToJson
             // Name <id>
 
             // Returns just the name
-            return text.Replace("<id>", "").Trim();
+            return text.Replace("<id>", "").Trim().ToCamelCase();
 
         }
         /// <summary>
@@ -54,7 +54,7 @@ namespace SpirvSpecToJson
                 if (!s.EndsWith("s"))
                     s += "s";
 
-                a[0] = s;
+                a[0] = s.ToCamelCase();
                 a[1] = "ID[]";
             }
 
@@ -75,7 +75,7 @@ namespace SpirvSpecToJson
                 if (text.Contains("See"))
                     a[0] = "ExtraOperands";
                 else
-                    a[0] = s;
+                    a[0] = s.ToCamelCase();
 
                 // Type: LiteralNumber
                 a[1] = "LiteralNumber[]";
@@ -101,6 +101,27 @@ namespace SpirvSpecToJson
                 // TODO
             }
             return s.ToString();
+        }
+
+        /// <summary>
+        /// Convert string into CamelCase
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static string ToCamelCase(this string s)
+        {
+            var test = s.Split();
+            var sx = new StringBuilder();
+            var st = new StringBuilder();
+            // Capitalize
+            foreach (var t in test)
+            {
+                sx.Append(char.ToUpper(t[0]) + t.Substring(1));
+                st.Append(sx);
+                sx.Clear();
+            }
+
+            return sx.ToString();
         }
     }
 }
